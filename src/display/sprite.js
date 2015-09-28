be953f6f6ec231682c42299028bb3a56a3dd6ba3
minimo <minimo.mobile@gmail.com>
2015-09-28 17:35:55 +0900

    add texture trimming

--- src/display/sprite.js
+++ src/display/sprite.js
@@ -20,6 +20,11 @@ phina.namespace(function() {
       this.height = height || this.image.domElement.height;
       this._frameIndex = 0;
 
+      this._frameTrimX = 0;
+      this._frameTrimY = 0;
+      this._frameTrimW = this.width;
+      this._frameTrimH = this.height;
+
       this.srcRect = {
         x: 0,
         y: 0,
@@ -45,22 +50,35 @@ phina.namespace(function() {
     },
 
     setFrameIndex: function(index, width, height) {
+      var sx = this._frameTrimX || 0;
+      var sy = this._frameTrimY || 0;
+      var sw = this._frameTrimW || (this.image.domElement.width-sx);
+      var sh = this._frameTrimH || (this.image.domElement.height-sy);
+
       var tw  = width || this.width;      // tw
       var th  = height || this.height;    // th
-      var row = ~~(this.image.domElement.width / tw);
-      var col = ~~(this.image.domElement.height / th);
+      var row = ~~(sw / tw);
+      var col = ~~(sh / th);
       var maxIndex = row*col;
       index = index%maxIndex;
       
       var x   = index%row;
       var y   = ~~(index/row);
-      this.srcRect.x = x*tw;
-      this.srcRect.y = y*th;
+      this.srcRect.x = sx+x*tw;
+      this.srcRect.y = sy+y*th;
       this.srcRect.width  = tw;
       this.srcRect.height = th;
 
       this._frameIndex = index;
 
+      return this;
+    },
+
+    setFrameTrimming: function(x, y, width, height) {
+      this._frameTrimX = x || 0;
+      this._frameTrimY = y || 0;
+      this._frameTrimW = width || this.image.domElement.width - this._frameTrimX;
+      this._frameTrimH = height || this.image.domElement.height - this._frameTrimY;
       return this;
     },
 
